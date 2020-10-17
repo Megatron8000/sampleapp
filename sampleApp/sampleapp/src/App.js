f87d684adc982b6a57bddd64a8ad4ba2c960d8a4
Barykin88 <barykin88@icloud.com>
2020-10-17 14:38:31 +0400

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Sidebar/sidebar.js
    create skeleton app styles

--- sampleApp/sampleapp/src/App.js
+++ sampleApp/sampleapp/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './App.css';
-import ContentArea from './components/ContentArea/contentArea';
-import Form from './components/Form/form';
-import Sidebar from './components/Sidebar/sidebar';
+import './App.css'
+// import ContentArea from './components/ContentArea/contentArea';
+// import Form from './components/Form/form';
+// import Sidebar from './components/Sidebar/sidebar';
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <ContentArea />
-      <Form />
+    <div className='app-wrapper'>
+        <div className='sidebar'>Sidebar</div>
+        <div className='content'>Content</div>
+        <div className='form'>Form</div>
     </div>
   );
 }

3cb50e824d0ebf167d753532341f66e0023ecfff
Barykin88 <barykin88@icloud.com>
2020-10-26 23:35:08 +0400

    add initial state

--- sampleApp/sampleapp/src/components/ContentArea/contentArea.js
+++ sampleApp/sampleapp/src/components/ContentArea/contentArea.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import classes from './contentArea.module.css'
+import Skill from './skillpoint'
+
 
 const ContentArea = () => {
-    return <div className={classes.content}>ContentArea</div>
+    return <div className={classes.content}>
+      <Skill />
+    </div>
 }
 export default ContentArea
---
+++ sampleApp/sampleapp/src/components/ContentArea/skillpoint.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './skillpoint.module.css'
+
+
+const Skill = () => {
+    return <div className={classes.cell}>
+        Some Skill
+    </div>
+}
+
+export default Skill

2688e6ed0512a2df25774c04f1394a49e809db67
Megatron8000 <barykin88@icloud.com>
2020-10-27 18:24:21 +0400

    Now links open in new tab

--- sampleApp/sampleapp/src/components/Sidebar/social.js
+++ sampleApp/sampleapp/src/components/Sidebar/social.js
@@ -9,19 +9,19 @@ import classes from './social.module.css'
 
 const SocialButtons = () => {
     return <div>
-        <a href='tel:+79967244224'>
+        <a href='tel:+79967244224' target="_blank" rel="noopener noreferrer">
             <img src={call} alt='phone' className={classes.buttons} />
         </a>
-        <a href='https://www.t.me/hollenteufel63'>
+        <a href='https://www.t.me/hollenteufel63' target="_blank" rel="noopener noreferrer">
             <img src={tele} alt='telegram' className={classes.buttons} />
         </a>
-        <a href='https://www.facebook.com/AlexanderBarykin88'>
+        <a href='https://www.facebook.com/AlexanderBarykin88' target="_blank" rel="noopener noreferrer">
             <img src={faceb} alt='facebook' className={classes.buttons} />
         </a>
-        <a href='https://www.instagram.com/hollenteufel63/'>
+        <a href='https://www.instagram.com/hollenteufel63/' target="_blank" rel="noopener noreferrer">
             <img src={inst} alt='instagram' className={classes.buttons} />
         </a>
-        <a href='https://github.com/Megatron8000'>
+        <a href='https://github.com/Megatron8000' target="_blank" rel="noopener noreferrer">
             <img src={github} alt='github' className={classes.buttons} />
         </a>
     </div>

c110353e021ba37458502393525493f93fc66744
Megatron8000 <barykin88@icloud.com>
2020-11-25 16:35:15 +0400

    form block

--- sampleApp/sampleapp/src/components/Form/form.js
+++ sampleApp/sampleapp/src/components/Form/form.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import classes from './form.module.css'
 
+
+let buttonAction = () => {
+    alert('Click')
+}
+
 const Form = () => {
-    return <div className={classes.form}>Form</div>
+    return <div className={classes.form}>
+        <h1 className={classes.header}>Add new skill</h1>
+        <div className={classes.center}>
+            <button onClick={buttonAction} className={classes.button}>Add skill!</button>
+        </div>
+    </div>
 }
 export default Form

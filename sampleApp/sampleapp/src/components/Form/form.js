d6b459437e9909ab3383ece5fd19b3426aa280fc
Megatron8000 <barykin88@icloud.com>
2020-12-08 13:49:52 +0400

    add form fields

--- sampleApp/sampleapp/src/components/Form/form.js
+++ sampleApp/sampleapp/src/components/Form/form.js
@@ -5,7 +5,6 @@ import classes from './form.module.css'
 let buttonAction = () => {
     alert('Click')
 }
-
 const Form = () => {
     return <div className={classes.form}>
         <h1 className={classes.header}>Add new skill</h1>

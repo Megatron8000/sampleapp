c99e6bbd6e78770be7b266e9a99b3d9fed9fe8db
Megatron8000 <barykin88@icloud.com>
2020-12-08 13:46:56 +0400

    add form fields

--- sampleApp/sampleapp/src/components/Form/form.js
+++ sampleApp/sampleapp/src/components/Form/form.js
@@ -10,6 +10,14 @@ const Form = () => {
     return <div className={classes.form}>
         <h1 className={classes.header}>Add new skill</h1>
         <div className={classes.center}>
+            <form>
+                <label>
+                    Name: <input type="text" name="name" />
+                </label>
+                <label>
+                    Skill: <input type="text" name="skill" />
+                </label>
+            </form>
             <button onClick={buttonAction} className={classes.button}>Add skill!</button>
         </div>
     </div>
